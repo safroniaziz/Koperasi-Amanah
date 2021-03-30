24906bfac12240b3214c4d59b555cd0e094c9503
safroniaziz <safroni.aziz@gmail.com>
2021-03-30 13:05:05 +0700

    add barcharts on operator dashboard

--- app/Http/Controllers/Operator/DashboardOperatorController.php
+++ app/Http/Controllers/Operator/DashboardOperatorController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Operator;
 
+use App\Anggota;
+use App\Berita;
 use App\Http\Controllers\Controller;
+use App\JenisTransaksi;
+use App\Transaksi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardOperatorController extends Controller
 {
@@ -13,6 +18,13 @@ class DashboardOperatorController extends Controller
     }
 
     public function dashboard(){
-        return view('backend/operator.dashboard');
+        $anggota = Count(Anggota::where('nm_anggota','!=','Koperasi')->get());
+        $berita = Count(Berita::all());
+        $jenis = Count(JenisTransaksi::where('status_jenis_transaksi','1')->get());
+        $simpanan = Transaksi::where('jenis_transaksi_id','1')->select(DB::raw('sum(jumlah_transaksi) as jumlah'))->first();
+        $datas = Anggota::rightJoin('transaksis','transaksis.anggota_id','anggotas.id')->where('jenis_transaksi_id','1')
+                            ->select('nm_anggota',DB::raw('sum(jumlah_transaksi) as jumlah'))
+                            ->groupBy('anggotas.id')->get();
+        return view('backend/operator.dashboard',compact('anggota','berita','jenis','simpanan','datas'));
     }
 }
--- resources/views/backend/operator/dashboard.blade.php
+++ resources/views/backend/operator/dashboard.blade.php
@@ -7,6 +7,14 @@
 @section('sidebar-menu')
     @include('backend/operator/sidebar')
 @endsection
+@push('styles')
+    <style>
+        #chartdiv {
+            width: 90%;
+            height: 500px;
+        }
+    </style>
+@endpush
 @section('content')
     <div class="callout callout-info ">
         <h4>SELAMAT DATANG!</h4>
@@ -18,5 +26,135 @@
         </p>
     </div>
 
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>{{ $anggota }}</h3>
 
+                    <p>Jumlah Anggota</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-users"></i>
+                </div>
+                <a href="" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-green">
+                    <div class="inner">
+                    <h3><sup style="font-size: 20px"></sup> {{ $berita }} </h3>
+
+                    <p>Jumlah Berita</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user"></i>
+                </div>
+                <a href="" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3> {{ $jenis }} </h3>
+
+                    <p>Jenis Transaksi</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3> Rp,{{ number_format($simpanan->jumlah) }} </h3>
+
+                    <p>Jumlah Simpanan Wajib</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-key"></i>
+                </div>
+                <a href="" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><i class="fa fa-calendar"></i>&nbsp;Tampilan Diagram Batang</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive">
+                    <h6 class="text-center text-bold">Jumlah Transaksi Simpanan Wajib Anggota</h6>
+                        @section('chart_data')
+                        chart.data = [
+                            @foreach ($datas as $data)
+                                {
+                                    "country": "{{ $data['nm_anggota'] }}",
+                                    "visits": {{ $data['jumlah'] }}
+                                },
+                            @endforeach
+                        ];
+                    @endsection
+                    <div id="chartdiv"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@push('scripts')
+    <script src="https://www.amcharts.com/lib/4/core.js"></script>
+    <script src="https://www.amcharts.com/lib/4/charts.js"></script>
+    <script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+    <script>
+    
+    am4core.ready(function() {
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    // Create chart instance
+    var chart = am4core.create("chartdiv", am4charts.XYChart);
+    chart.scrollbarX = new am4core.Scrollbar();
+    // Add data
+    @yield('chart_data')
+    // Create axes
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "country";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.verticalCenter = "middle";
+    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.tooltip.disabled = true;
+    categoryAxis.renderer.minHeight = 110;
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.minWidth = 50;
+    // Create series
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.sequencedInterpolation = true;
+    series.dataFields.valueY = "visits";
+    series.dataFields.categoryX = "country";
+    series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
+    series.columns.template.strokeWidth = 0;
+    series.tooltip.pointerOrientation = "vertical";
+    series.columns.template.column.cornerRadiusTopLeft = 10;
+    series.columns.template.column.cornerRadiusTopRight = 10;
+    series.columns.template.column.fillOpacity = 0.8;
+    // on hover, make corner radiuses bigger
+    var hoverState = series.columns.template.column.states.create("hover");
+    hoverState.properties.cornerRadiusTopLeft = 0;
+    hoverState.properties.cornerRadiusTopRight = 0;
+    hoverState.properties.fillOpacity = 1;
+    series.columns.template.adapter.add("fill", function(fill, target) {
+    return chart.colors.getIndex(target.dataItem.index);
+    });
+    // Cursor
+    chart.cursor = new am4charts.XYCursor();
+    }); // end am4core.ready()
+    </script>
+@endpush
